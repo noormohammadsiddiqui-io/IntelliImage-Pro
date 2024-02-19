14894ddcf67a81203cf12366faef4ca2eef1a0af
Noor Mohammad Siddiqui <noorprofessionalmail@gmail.com>
2024-02-19 13:22:27 +0530

    Preset updated

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="IntelliImage-Pro"
       options={{
         multiple: false,
         resourceType: "image",
